aac8b635930c9e6c5c998b8e29879c1d7f7f9ef6
MahtabAmeli <mahtab.ameli@gmail.com>
2023-11-10 12:35:33 -0500

    Removed tests from main method of PaymentProcessor, and made changes for better testing.

--- src/main/java/org/lab2/PaymentProcessor.java
+++ src/main/java/org/lab2/PaymentProcessor.java
@@ -1,134 +1,95 @@
 package org.lab2;
 
+import java.util.Map;
+
 /**
  * Instances of PaymentProcessor are responsible for processing transactions of items in shopping cart.
+ *
  * @author: Mahtab Ameli
  */
 public class PaymentProcessor {
 
     /**
-     * The shopping cart for which payment is being processed.
-     */
-    private Cart cart;
-
-    /**
-     * Unique 5-digit transaction number for each payment processing attempt.
-     * todo make test for transaction number correctly incrementing
+     * Counter for generating unique transaction numbers.
      */
     private static int transactionCounter = 10000;
 
-
-
     /**
-     * Constructor for the class.
-     * @param cart to be checked out.
+     * Processes the payment for the given shopping cart.
+     *
+     * @param cart The shopping cart to process.
      */
-    public PaymentProcessor(Cart cart) {
-        this.cart = cart;
-    }
-
-
-
-    //todo add transaction number to each payment processing attempt
-    public boolean processPayment() {
-
-        if (cart.isEmpty()) {
-            System.out.println("\nPAYMENT CANNOT BE PROCESSED: SHOPPING CART IS EMPTY!\n");
-            return false;
+    public static void processPayment(Cart cart) {
+        // Check if the cart is null or empty
+        if (cart == null || cart.getItems().isEmpty()) {
+            System.out.println("PAYMENT CANNOT BE PROCESSED: SHOPPING CART IS EMPTY!");
+            return;
         }
 
-        //todo check if cart items are available in inventory before attempting payment processing
-        // if an item is not available in inventory, remove it from the cart and prompt user
+        // Generate a unique transaction number
+        int transactionNumber = generateTransactionNumber();
+        // Calculate the total amount for the items in the cart
+        double totalAmount = cart.calculateTotal();
 
-        // process payment if items are available in inventory
-        String transactionNumber = generateTransactionNumber(); 
-        double cartTotal = cart.calculateTotal();
-        String line = "*******************************************************\n";
-        System.out.println(line + "Transaction #: " + transactionNumber + "\n");
-        System.out.println("Processing payment of $" + cartTotal + "...\n\n" + cart);
+        System.out.println("*******************************************************");
+        System.out.println("Transaction #: " + transactionNumber);
+        System.out.println("\nProcessing payment of $" + totalAmount + "...\n");
 
-        // todo authorize payment method
+        // Display the contents of the cart
+        printCartContents(cart);
 
-        boolean paymentApproved = authorizePaymentMethod();
-        transactionCounter++;
-
-        // if payment method approved
-        if (paymentApproved) {
-            System.out.println("***** PAYMENT APPROVED *****\n" + line);
-            boolean inventoryUpdated =  updateInventory();
-            return true;
+        // Check if the payment is approved
+        if (isPaymentApproved()) {
+            System.out.println("***** PAYMENT APPROVED *****");
+            // Update the inventory after a successful payment
+            updateInventory(cart);
+        } else {
+            System.out.println("***** PAYMENT DECLINED *****");
         }
 
-        // if payment method not approved
-        System.out.println("***** PAYMENT DENIED *****\n" + line);
-        return false;
+        System.out.println("*******************************************************");
     }
-
-
-
-    //todo ask group if inventory check should be done from here
-    private boolean checkInventory() {
-        return false;
-    }
-
-
 
     /**
-     * Updates inventory by removing items for which payment was successful.
-     * // todo ask group about inventory implementation
+     * Generates a unique transaction number.
+     *
+     * @return The generated transaction number.
      */
-    private boolean updateInventory() {
-        // remove items that were successfully paid for form cart
-        return false;
+    public static int generateTransactionNumber() {
+        return transactionCounter++;
     }
 
-
-
     /**
-     * Returns true if payment method is successful.
-     * todo ask group if should do different payment methods for this milestone
-     * todo ask group about user's payment method
+     * Displays the contents of the shopping cart.
+     *
+     * @param cart The shopping cart to display.
      */
-    private boolean authorizePaymentMethod() {
-        return false;
+    private static void printCartContents(Cart cart) {
+        System.out.println(cart.toString());
     }
 
-
-
     /**
-     * Generates a unique 5-digit transaction number returned as a string.
+     * Simulates payment approval logic.
+     *
+     * @return true if the payment is approved, false otherwise.
      */
-    private String generateTransactionNumber() {
-        return String.format("%05d", transactionCounter);
+    private static boolean isPaymentApproved() {
+        // Simulate payment approval logic in future milestones
+        return true;
     }
 
-
     /**
-     * for temporary "testing".
-     * @param args
+     * Updates the inventory after a successful payment.
+     *
+     * @param cart The shopping cart containing items to update the inventory.
      */
-    public static void main(String[] args) {
-        /**
-         * case 1: cart is empty.
-         */
-        Cart cart_empty = new Cart();
-        PaymentProcessor p = new PaymentProcessor(cart_empty);
-        p.processPayment();
-
-        /**
-         * case 2: cart has 1 item.
-         */
-        Cart cart_1 = new Cart();
-        cart_1.addBook(new Book("ISBN_1", "BOOK NAME 1", "AUTHOR NAME 1", "PUBLISHER NAME 1", 1, 30), 1);
-        PaymentProcessor p2 = new PaymentProcessor(cart_1);
-        p2.processPayment();
-
-        /**
-         * case 2: cart has 2 items.
-         */
-        Cart cart_2 = new Cart();
-        cart_2.addBook(new Book("ISBN_2", "BOOK NAME 2", "AUTHOR NAME 2", "PUBLISHER NAME 2", 2, 50), 2);
-        PaymentProcessor p3 = new PaymentProcessor(cart_2);
-        p3.processPayment();
+    private static void updateInventory(Cart cart) {
+        BookStoreManagement bookStoreManagement = new BookStoreManagement();
+        for (Map.Entry<Book, Integer> entry : cart.getItems().entrySet()) {
+            Book book = entry.getKey();
+            int quantity = entry.getValue();
+            // Update the quantity of each book in the inventory
+            bookStoreManagement.updateQuantity(book.getIsbn(), -quantity);
+        }
     }
 }

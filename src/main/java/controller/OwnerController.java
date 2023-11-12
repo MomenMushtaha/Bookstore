4e08a17ec2b22f855f3889858ada2cdb0c8ee61b
kyler <vergekyler@gmail.com>
2023-11-12 17:43:32 -0500

    html files

--- src/main/java/controller/OwnerController.java
+++ src/main/java/controller/OwnerController.java
@@ -13,7 +13,7 @@ public class OwnerController {
     private BookStoreManagementRepository bookStoreManagementRepository;
 
 
-    @PostMapping
+    @PostMapping("/owner")
     public Owner createOwner(@RequestBody Owner newOwner){
 
         BookStoreManagement ownerBookStore = new BookStoreManagement();
@@ -25,6 +25,15 @@ public class OwnerController {
     public Owner getOwner(@PathVariable Long id) {
         return ownerRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid owner ID"));
+    }
+
+    @PostMapping("/owner/{id}/addBookFront")
+    public String addBook(@PathVariable Long id, @ModelAttribute Book book){
+        BookStoreManagement ownerBookStore = bookStoreManagementRepository.findById(id).orElseThrow();
+        ownerBookStore.addBook(book);
+        System.out.println("Test add book");
+        bookStoreManagementRepository.save(ownerBookStore);
+        return "redirect:/owner";
     }
 
     //public BookStoreManagement getBookStore(){

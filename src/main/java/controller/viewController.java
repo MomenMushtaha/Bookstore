b35fbdba4eadf3772224eefc123566230c0e3f5b
kyler <vergekyler@gmail.com>
2023-11-25 14:30:44 -0500

    changed viewControllers annotation back to Controller. Fixes view

--- src/main/java/controller/viewController.java
+++ src/main/java/controller/viewController.java
@@ -10,11 +10,12 @@ import repository.CartRepository;
 import repository.CustomerRepository;
 import repository.OwnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
-@RestController
+@Controller
 public class viewController {
     // Injecting dependencies for the used repositories
     @Autowired

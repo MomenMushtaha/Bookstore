299ccdd0dc053984ea31c451d41b01587075a73a
Hamza Zafar <Hamzazfr10@outlook.com>
2023-11-26 13:49:02 -0500

    Fixed issue with customer and cart

--- src/test/java/mockmvc/SmokeTest.java
+++ src/test/java/mockmvc/SmokeTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import controller.CustomerController;
 import controller.OwnerController;
-import application.AccessingDataJpaApplication;
+import model.AccessingDataJpaApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
